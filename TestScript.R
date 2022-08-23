f1da685b1a4f575f306fc993beb38bebb3e34b1e
ahart1 <arhart555@gmail.com>
2022-08-23 16:10:12 -0400

    Update debugging section.

--- TestScript.R
+++ TestScript.R
@@ -15,17 +15,22 @@ autoREADME(dirREADME = "TestRepo/TestFolder")
 
 ##### Debugging #####
 # If the GitHub action does not work try:
-# 1) Check that the personal action token is still active
-# 1a) GitHub profile -> Settings -> <> Developer settings -> Personal access tokens -> check expiration for token
-# 1b) If expired, create a new token & copy token
-# 1c) Navigate to miscFunctions -> Settings -> Secrets -> Actions -> Edit the personal access secret with the new token
-# 2) Check that all repos for which README files should be generated have read/write permission for actions (default = only read permission)
-# 2a) Check GitHub repo -> Settings -> Actions -> General -> Workflow permissions
-# 2b) Make sure "Read and write permissions" option is selected
-# 3) Check frequency with which action is triggered
-# 3a) Currently set up to run weekly, this can be edited by:
-# 3b) Change schedule using 'cron' in YAML file OR
-# 3c) Uncomment the 'push: branches: [ master ]' lines in the YAML so action run on every push (CAUTION: frequent pushes could result in running out of GitHub action minutes!)
+  # 1) Check that the personal action token is still active
+    # 1a) GitHub profile -> Settings -> <> Developer settings -> Personal access tokens -> check expiration for token
+    # 1b) If expired, create a new token & copy token
+    # 1c) Navigate to miscFunctions -> Settings -> Secrets -> Actions -> Edit the personal access secret with the new token
+  # 2) Check that all repos for which README files should be generated have read/write permission for actions (default = only read permission)
+    # 2a) Check GitHub repo -> Settings -> Actions -> General -> Workflow permissions
+    # 2b) Make sure "Read and write permissions" option is selected
+  # 3) Check frequency with which action is triggered
+    # 3a) Currently set up to run weekly, this can be edited by:
+    # 3b) Change schedule using 'cron' in YAML file OR
+    # 3c) Uncomment the 'push: branches: [ master ]' lines in the YAML so action run on every push (CAUTION: frequent pushes could result in running out of GitHub action minutes!)
+  # 4) Check that your existing README has a File and Folder header and the first line of the table:
+    ##  # File/Folder Title
+    ##  | File        | Description | # Swap out Folder in this line for the folder section
+    ##  | ----------- | ----------- |
+    # 4a) NOTE: The README causing errors may be only on GitHub if you haven't pulled changes recently!
 
 ##### Resources #####
 # Simpler example: https://blog--simonpcouch.netlify.app/blog/r-github-actions-commit/
